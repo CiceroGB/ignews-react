369cb62f55f6e228f9257261e8d7c96eea8033a4
Cicero Gasparini Brunelli <brunelli.cg@gmail.com>
2022-03-13 00:27:50 -0300

    chore: SubscribeButton

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { GetServerSideProps } from 'next'
 import Head from 'next/head'
+import { SubscribeButton } from '../components/SubscribeButton'
 import { stripe } from '../services/stripe'
 
 import styles from './home.module.scss';
@@ -18,18 +19,20 @@ export default function Home({ product }: HomeProps) {
         <title>Home | ig.news</title>
       </Head>
       <main className={styles.contentContainer}>
-      <section className={styles.hero}>
+        <section className={styles.hero}>
           <span>👏 Hey, Welcome</span>
           <h1> News about the <span>React</span> world.</h1>
           <p>
             Get acess to all publications <br />
             <span>for {product.amount} month</span>
           </p>
+          <SubscribeButton />
         </section>
 
         <img src="/images/avatar.svg" alt="Girl coding" />
 
       </main>
+
     </>
   )
 }
