3e5bb9520e56890a254c93a68884b243d7172d00
Cicero Gasparini Brunelli <brunelli.cg@gmail.com>
2022-03-13 00:07:46 -0300

    chore: stripe and getServerSideProps from Stripe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,15 @@
+import { GetServerSideProps } from 'next'
 import Head from 'next/head'
+import { stripe } from '../services/stripe'
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string
+    amount: number,
+  }
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -8,8 +17,27 @@ export default function Home() {
       </Head>
       <h1 >
 
-        Next on
+        {product.amount}
     </h1>
     </>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve('price_1KchISHOpN75BigQwx08xaQj', {
+    expand: ['product']
+  })
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD'
+    }).format(price.unit_amount / 100)
+  }
+
+  return {
+    props: {
+      product
+    }
+  }
 }
